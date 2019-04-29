0d96b8342b2a5131809b0f589d9257044164b30c
Saurabh Srivastava <vvasu001@gmail.com>
2019-04-29 13:50:14 +0530

    Add files via upload

---
+++ Theme Projects/Theme Project One/js/front.js
@@ -0,0 +1,10 @@
+$(function () {
+
+	const navIcon = $('#nav-pull-icon');
+
+	navIcon.click(() => {
+		const displayFrontHeaderSmall = $('#front-header-nav-small');
+		displayFrontHeaderSmall.toggle(300);
+	});
+
+});
